cfdad9cabe8a323b8631258aed40c03878c8f7e9
metam478 <metam0268@gmail.com>
2024-12-01 00:04:05 +0100

    add initial packages

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,9 @@
+<script setup>
+import postsQueries from '@/queries/posts.ts'
+
+const posts = postsQueries.usePosts()
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
